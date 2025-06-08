521d5809aea7ada7d04210726a3a465a6cbd3efe
Youngboi3339800 <saharholmes@gmail.com>
2025-06-07 20:41:00 -0400

    Added contributing.md, todo.md and installed ninja

---
+++ TODO.md
@@ -0,0 +1,2 @@
+# TODO
+- [ ] Get the physac library working (2d physics engine)
---
+++ source/raylibImplementations.cpp
@@ -0,0 +1,5 @@
+#define RAYLIB_NO_MAIN
+#define PLATFORM_DESKTOP
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
